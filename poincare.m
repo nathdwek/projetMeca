8997fe3a661784e1a32843f3a4abb09aa9c2e54f
nathHome <nathdwek@ulb.ac.be>
2013-12-04 22:39:32 +0100

    renames

--- poincare.m
+++ poincare.m
@@ -1,46 +1,43 @@
 clear all
 
 global g L omega C l
-g=0;
+g=9.81;
 L=1;
 t0=0;
-
+tGlob=[];
+yGlob=[];
 
 
 
 %C'est ici que ça se passe
 l=0.4;
-rebondsMax=50;
+rebondsMax=10000;
 omega=0;
 y0=0.6;
-yDot0=0;
+yDot0=-2;
 x0=0;
 xDot0=1;
+dynamicView=1;
 C=1;
-%%%%%%%%%%%%%%%%%%%%%%%%%
-periode=0.005;
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+tStep=4*L/xDot0
 firstPick=0;
-tG=[];
-yG=[];
 
 
 
+
+
+
+options = odeset('Events',@nextRebound,'RelTol',1e-8);
 for i=1:rebondsMax
-   rebonds=i
-   [t y t0 x0 y0 xDot0 yDot0 firstPick]=oneRebound2(t0, firstPick, periode, x0, y0, xDot0, yDot0);
-   t=t
-   tG=[tG;t];
-   yG=[yG;y];
+   [t y t0 x0 y0 xDot0 yDot0 firstPick]=oneRebound2(t0, firstPick, tStep, x0, y0, xDot0, yDot0);
+   tGlob=[tGlob;t];
+   yGlob=[yGlob;y];
 end
 
 figure('NumberTitle','on','Name','Section Poincare','Renderer','OpenGL','Color','w','Position',[200 200 600 600])
-plot([yG(1:end,1)],[yG(1:end,2)],"linestyle", "none", "Marker", "*", "MarkerSize",3)
-axis("auto")
+plot(yGlob(1:end,1),yGlob(1:end,3),"linestyle", "none", "Marker", "*", "MarkerSize",3);
 axis([-1.2 1.2])
 grid on; box on;
 hold on;
-disp("Taille de léchantillon: ");disp(length(yG));
-
-for i=1:length(tGlob)-1
-   disp(tGlob(i)-tGlob(i+1)+periode);
-end
+disp("Taille de léchantillon: ");disp(length(yGlob));
