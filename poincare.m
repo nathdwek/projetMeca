63580ca3759f80ca9d20e777180ea81258dc6a00
nathULB <nathdwek@ulb.ac.be>
2013-12-06 14:01:52 +0100

    periode adaptée en x et y

--- poincare.m
+++ poincare.m
@@ -10,8 +10,8 @@ yGlob=[];
 
 
 %C'est ici que ça se passe
-l=0.4;
-rebondsMax=10000;
+l=0.1;
+rebondsMax=1000;
 omega=0;
 y0=0.6;
 yDot0=-2;
@@ -20,7 +20,7 @@ xDot0=1;
 dynamicView=1;
 C=1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-periode=4*L/abs(xDot0);
+periode=findPeriode("y",x0,y0,xDot0,yDot0);
 firstPick=0;
 
 
@@ -34,10 +34,22 @@ for i=1:rebondsMax
    tGlob=[tGlob;t];
    yGlob=[yGlob;y];
 end
+yGlob=yGlob(:,[2 4]);
+
+for i=1:length(tGlob)-1
+   if abs(tGlob(i+1)-tGlob(i)-periode)>1E-12
+      i=i
+      diff=tGlob(i+1)-tGlob(i)-periode
+   end
+end
 
 figure('NumberTitle','on','Name','Section Poincare','Renderer','OpenGL','Color','w','Position',[200 200 600 600])
-plot(yGlob(1:end,1),yGlob(1:end,3),"linestyle", "none", "Marker", "*", "MarkerSize",3);
-axis([-1.2 1.2])
+axis([-1.1 1.1 min(yGlob(:,2))-0.1 max(yGlob(:,2))+0.1])
 grid on; box on;
 hold on;
+for i=1:length(yGlob)
+   line([yGlob(i,1)], [yGlob(i,2)], "Marker", ".", "MarkerSize",6);
+   pause(0.1);
+end
 disp("Taille de léchantillon: ");disp(length(yGlob));
+
